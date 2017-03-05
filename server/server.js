c78b75fdcbede79c3b6d917a4b1d9a28bc80bc83
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-03-05 11:40:12 +0200

    server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+
+var UserController = require('./Usres/userController.js')
+
+//middleware
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.json());
+
+
+// Connect to Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/company');
+var db = mongoose.connection;
+
+app.post('' ,)
+app.post('',)
+app.get('',)
+
+
+
+app.listen(process.env.PORT || 3000);
+console.log('Running on port 3000...');
+
+module.exports = app;

b219fe919b6249e3cca928e75ccfa5ee4e60ae4c
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-03-18 17:03:16 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,8 +41,8 @@ import { CreatcompanyComponent } from './creatcompany/creatcompany.component';
   {path: 'signin',component: SigninComponent},
   {path: 'aboutus',   component: AboutusComponent},
   {path: 'company',   component: CompanyComponent},
-  {path: 'task',   component: TaskComponent},
-  {path: 'schedule',   component: ScheduleComponent}
+  {path: 'task',   component: TaskComponent}
+  //{path: 'schedule',   component: ScheduleComponent}
   ]),
     FlashMessagesModule
   ],

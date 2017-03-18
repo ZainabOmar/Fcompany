038c440535d628ff3f19d4deb893b594cced68a8
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-03-18 17:45:20 +0200

    fix server

--- server/server.js
+++ server/server.js
@@ -7,12 +7,13 @@ const cors = require('cors');
 var UsersController = require('./Users/userController.js');
 var CompanyController = require('./Company/companyController.js');
 var scheduleController = require('./schedule/scheduleController.js');
+var TaskController = require('./Task/taskController.js');
 
 //middleware
 app.use(express.static(__dirname + '/../dist'));
 app.use(bodyParser.json());
 
- app.use(cors());
+app.use(cors());
 
 
 // Connect to Mongoose
@@ -20,16 +21,14 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/company');
 var db = mongoose.connection;
 
 
-
-
 app.post('/api/user/signin' , UsersController.handleUsers.signin);
 app.post('/api/user/signup', UsersController.handleUsers.signup);
 app.get('/api/users', UsersController.handleUsers.getUsers)
 
 
-app.post('/api/company', CompanyController.handelCompany.addCompany);
-app.get('/api/companys', CompanyController.handelCompany.showCompany);
-
+app.post('/api/company', CompanyController.handleCompany.addCompany);
+// app.get('/api/company', CompanyController.handleCompany.showAllCompanies);
+app.get('/api/company/:userId', CompanyController.handleCompany.getCompany);
 
 app.post('/api/schedule/add',scheduleController.handlesched.add);
 app.post('/api/schedule/meeting/add',scheduleController.handlesched.setmeeting);
@@ -37,9 +36,8 @@ app.get('/api/schedule/:id',scheduleController.handlesched.get);
 app.get('/api/schedule/meeting/:id',scheduleController.handlesched.getusers);
 app.delete('/api/schedule/appoitment',scheduleController.handlesched.delete);
 
-
-
-
+app.get('/api/task', TaskController.handleTask.getTask);
+app.post('/api/task', TaskController.handleTask.addTask);
 
 app.listen(process.env.PORT || 3000);
 console.log('Running on port 3000...');

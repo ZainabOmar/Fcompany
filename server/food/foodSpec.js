1da9237a3e8384828057f3af9331df3c384ea7ee
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-03-21 18:00:39 +0200

    food and schedule test

---
+++ server/food/foodSpec.js
@@ -0,0 +1,40 @@
+const app = require('../server.js')
+const request = require('supertest')
+const expect = require('chai').expect
+
+describe('food', function () {
+  it('Add food', function (done) {
+    request(app)
+    .post('/api/food/add/')
+    .send({
+      dishName: 'chinese',
+      type: 'chinese',
+      foodtime: 'dinner'
+    })
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201)
+    .end(function (err, resp) {
+      if (err) {
+        throw new Error(err)
+      }
+      expect(resp.body).to.be.an('object')
+    })
+    done()
+  })
+
+  it('Get food', function (done) {
+    request(app)
+    .get('/api/food/')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, resp) {
+      if (err) {
+        throw new Error(err)
+      }
+      expect(resp.body).to.be.an('object')
+    })
+    done()
+  })
+})

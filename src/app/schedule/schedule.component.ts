99db1a936cc1e8e2ae9efbd82a2d17cc668c2227
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-03-22 12:39:40 +0200

    refactor html

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ScheduleService } from '../services/schedule.service';
 import {FlashMessagesService} from 'angular2-flash-messages';
 import { Router } from '@angular/router';
-
+import { AuthService } from '../services/auth.service'
 
 
 @Component({
@@ -11,36 +11,41 @@ import { Router } from '@angular/router';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
-    info = false;
-    showSch = false;
-    title: String;
-    description: String;
-    date : Date ;
-    starttime : String;
-    endtime : String;
-    todos: Object[] = [];
-     
+  info = false;
+  showSch = false;
+  title: String;
+  description: String;
+  date : Date ;
+  starttime : String;
+  endtime : String;
+  todos: Object[] = [];
+  
   constructor(
+    private authService: AuthService,
     private flashMessage:FlashMessagesService,
     private scheduleservice : ScheduleService,
     private router: Router
-  ) {}
+    ) {}
 
   ngOnInit() {
-    // this.todos = [];
     this.scheduleservice.getSchedule(localStorage.getItem("user-id")).subscribe(data => {
       this.todos = data;
       console.log(this.todos)
     })
   }
-  
+
+  logout() {
+    this.authService.logout()
+    this.router.navigate(['/']);
+  }
+
   div_hide(){
     document.getElementById('aya').style.display = "none";
     if(this.title && this.date )
-    this.info = !this.info
+      this.info = !this.info
   }
 
- //Function To Display Popup
+  //Function To Display Popup
   div_show() {
     document.getElementById('aya').style.display = "block";
     this.showSch = false;
@@ -61,5 +66,5 @@ export class ScheduleComponent implements OnInit {
       if(data){
         this.flashMessage.show('task add well', {cssClass: 'alert-success', timeout: 3000});
       }})
-    }
+  }
 }
